70a41b891c8c5c2d9ce2996fa73cad3b7b0532bc
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-03-18 18:08:29 +0100

    analyse

--- src/steps/step1_data_ingestion.py
+++ src/steps/step1_data_ingestion.py
@@ -2,7 +2,6 @@ from zenml import step
 from src.components.data_ingestion import DataIngestion
 from src.config.get_config import ConfigManager
 from zenml.logger import get_logger
-from roboflow import Dataset
 
 logger = get_logger(__name__)
 STAGE_NAME = "Data Ingestion"
@@ -22,7 +21,7 @@ class DataIngestionStep:
 
 
 @step
-def data_ingestion() -> Dataset:
+def data_ingestion() -> Any:
     logger.info(f"\33[33m>>>>> 1️⃣ {STAGE_NAME}📀 step has started 🏁🏁 <<<<<\33[0m")
     obj = DataIngestionStep()
     dataset = obj.main()
