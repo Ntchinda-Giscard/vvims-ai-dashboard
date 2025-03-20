f1f3db1ae789416fd19609c4aafe3e97656f649e
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-03-20 07:36:05 +0100

    add load materializers for roboflow dataset

--- src/materializers/roboflow_materializers.py
+++ src/materializers/roboflow_materializers.py
@@ -11,16 +11,16 @@ class RoboflowDatasetMaterializer(BaseMaterializer):
     ASSOCIATED_TYPES = (roboflow.core.dataset.Dataset,)
     ASSOCIATED_ARTIFACT_TYPE = ArtifactType.DATA
 
-    def load(self, dataset: Type[roboflow.core.dataset.Dataset]) -> roboflow.core.dataset.Dataset:
+    def load(self, data_type: Type[roboflow.core.dataset.Dataset]) -> roboflow.core.dataset.Dataset:
         """Load the dataset from the artifact store."""
         # with self.artifact_store.open(os.path.join(self.uri, 'metadata.json'), 'r') as f:
         #     metadata = json.load(f)
         # # Use available methods to reconstruct the dataset
         # dataset = roboflow.core.dataset.Dataset.from_metadata(metadata)
         dataset = sv.DetectionDataset.from_yolo(
-            images_directory_path=f"{dataset.location}/train/images",
-            annotations_directory_path=f"{dataset.location}/train/labels",
-            data_yaml_path=f"{dataset.location}/data.yaml"
+            images_directory_path=f"{data_type.location}/train/images",
+            annotations_directory_path=f"{data_type.location}/train/labels",
+            data_yaml_path=f"{data_type.location}/data.yaml"
         )
         return dataset
 
