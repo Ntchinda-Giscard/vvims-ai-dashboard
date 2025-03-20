4facde2bd6739d784c804cd3614a9986fe00dc8b
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-03-20 07:19:32 +0100

    add load materializers for roboflow dataset

--- src/materializers/roboflow_materializers.py
+++ src/materializers/roboflow_materializers.py
@@ -4,17 +4,24 @@ from typing import Type
 from zenml.materializers.base_materializer import BaseMaterializer
 from zenml.enums import ArtifactType
 import roboflow
+import supervision as sv
+
 
 class RoboflowDatasetMaterializer(BaseMaterializer):
     ASSOCIATED_TYPES = (roboflow.core.dataset.Dataset,)
     ASSOCIATED_ARTIFACT_TYPE = ArtifactType.DATA
 
-    def load(self, data_type: Type[roboflow.core.dataset.Dataset]) -> roboflow.core.dataset.Dataset:
+    def load(self, dataset: Type[roboflow.core.dataset.Dataset]) -> roboflow.core.dataset.Dataset:
         """Load the dataset from the artifact store."""
-        with self.artifact_store.open(os.path.join(self.uri, 'metadata.json'), 'r') as f:
-            metadata = json.load(f)
-        # Use available methods to reconstruct the dataset
-        dataset = roboflow.core.dataset.Dataset.from_metadata(metadata)
+        # with self.artifact_store.open(os.path.join(self.uri, 'metadata.json'), 'r') as f:
+        #     metadata = json.load(f)
+        # # Use available methods to reconstruct the dataset
+        # dataset = roboflow.core.dataset.Dataset.from_metadata(metadata)
+        dataset = sv.DetectionDataset.from_yolo(
+            images_directory_path=f"{dataset.location}/train/images",
+            annotations_directory_path=f"{dataset.location}/train/labels",
+            data_yaml_path=f"{dataset.location}/data.yaml"
+        )
         return dataset
 
     def save(self, dataset: roboflow.core.dataset.Dataset) -> None:
